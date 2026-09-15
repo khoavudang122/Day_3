19c0e9457bb6bdd5f39dbe1753d9d530db54ed8c
Vũ Đăng Khoa <Khoavudang12@gmail.com>
2026-09-15 21:00:55 +0700

    Bài 2 easy mảng dynamic

---
+++ Bai2_dynamic_arr.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+
+int main(void) {
+    uint16_t num;
+    printf("Nhập num: ");
+    scanf("%"SCNu16, &num);
+    uint16_t *adc_buffer = malloc(num * sizeof(uint16_t));
+    uint16_t sum = 0;
+    uint16_t *reset = adc_buffer;
+    // kiểm tra HEAP
+    if (adc_buffer == NULL) return 1;
+    uint16_t *end_ptr = adc_buffer + num;
+    
+    while (adc_buffer <= end_ptr-1){
+        scanf("%"SCNu16,adc_buffer);
+        adc_buffer +=1;
+    }
+
+    while (reset <= end_ptr-1){
+        sum += *reset;
+        reset +=1;
+    }
+    printf("SUM: %"PRIu16"",sum);
+    free(adc_buffer);
+    free(reset);
+    free(end_ptr);
+}
