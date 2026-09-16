9d4a4b35aa00488e8e284a3c6d43250e64a270fe
Vũ Đăng Khoa <Khoavudang12@gmail.com>
2026-09-16 08:56:07 +0700

    Bài 5 advanced sử dụng bộ nhớ cấp phát động cho struct để ghi và in

---
+++ Bai5_sensor_struct_heap.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+#include<stdlib.h>
+
+typedef struct {
+    uint16_t rpm;
+    float temperature;
+    float current;
+
+} SensorData;
+
+void print(SensorData* ptr){
+    printf("%.2f\n",ptr->temperature);
+    printf("%.2f\n",ptr->current);
+    printf("%"PRIu16"\n",ptr->rpm);
+}
+
+int main(void) {
+    SensorData *sd_ptr = malloc(sizeof(SensorData));
+    if (sd_ptr == NULL) return 1;
+    scanf("%f",&sd_ptr->temperature);
+    scanf("%f",&sd_ptr->current);
+    scanf("%"SCNu16,&sd_ptr->rpm);
+
+    print(sd_ptr);
+    return 0;
+    
+}
