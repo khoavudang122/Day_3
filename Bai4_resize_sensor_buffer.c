7f73084740785a499dbb6515203a9f6f7538146d
Vũ Đăng Khoa <Khoavudang12@gmail.com>
2026-09-16 08:44:28 +0700

    chỉnh sửa lại poiter arr do quên +1

--- Bai4_resize_sensor_buffer.c
+++ Bai4_resize_sensor_buffer.c
@@ -21,10 +21,19 @@ int main(void) {
         printf("Bạn muốn mở rộng ra thành bao nhiêu? ");
         scanf("%d",&new_num);
         arr = realloc(arr,new_num * sizeof(uint16_t));
+        uint16_t *ptr = arr;
+        if ( ptr == NULL ) return 1;
         for (int i=0; i < new_num; i++){
-        scanf("%"SCNu16,arr);
+        scanf("%"SCNu16,ptr);
+        ptr +=1;
+        }
+        for (int i=0; i < new_num; i++){
+        printf("%"PRIu16"",*arr);
+        arr +=1;
+        }
+        free(ptr);
     }
-    }
+
     else return 1;
     free(arr);
     return 0;
