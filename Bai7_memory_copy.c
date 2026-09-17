83d9f09b1ca09b3c752014e71c5941256287a899
Vũ Đăng Khoa <Khoavudang12@gmail.com>
2026-09-17 19:33:09 +0700

    Cập nhật lại giải phóng con trỏ src và des

--- Bai7_memory_copy.c
+++ Bai7_memory_copy.c
@@ -12,12 +12,14 @@ int main(void) {
 
     uint8_t *source = malloc(num*sizeof(uint8_t));
     if (source ==NULL) return 1;
+    uint8_t *ptr_2 = source;
     uint8_t *ptr = source; 
     for (int i=0;i < num; i++ ){
         scanf("%"SCNu8,ptr);
         ptr++;
     }
     uint8_t *destination = calloc(num+1,sizeof(uint8_t));
+    uint8_t *ptr_3 = destination;
     for (int i=0;i<num;i++){
         destination = source;
         printf("%"PRIu8"\n",*destination);
@@ -25,7 +27,7 @@ int main(void) {
         source++;
     }
 
-    free(destination);
-    free(source);
+    free(ptr_2);
+    free(ptr_3);
 
 }
