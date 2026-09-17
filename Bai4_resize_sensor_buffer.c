bcf70a32c4bb5947a7a4f0c55bfee4c6bc3e55e1
Vũ Đăng Khoa <Khoavudang12@gmail.com>
2026-09-17 19:48:18 +0700

    Cập nhật lỗi memory leak và lỗi sai của  hàm nhập ban đầu

--- Bai4_resize_sensor_buffer.c
+++ Bai4_resize_sensor_buffer.c
@@ -9,9 +9,10 @@ int main(void) {
     int num;
     scanf("%d",&num);
     uint16_t *arr = malloc(num * sizeof(uint16_t));
-    
+    uint16_t *inp_ptr = arr;
     for (int i=0; i<num; i++){
-        scanf("%"SCNu16,arr);
+        scanf("%"SCNu16,inp_ptr);
+        inp_ptr++;
     }
     uint8_t flag;
     printf("Bạn có muốn mở rộng size không?(1:có , 2;không): ");
@@ -23,6 +24,7 @@ int main(void) {
         arr = realloc(arr,new_num * sizeof(uint16_t));
         uint16_t *ptr = arr;
         if ( ptr == NULL ) return 1;
+        inp_ptr = arr;
         for (int i=0; i < new_num; i++){
         scanf("%"SCNu16,ptr);
         ptr +=1;
@@ -31,10 +33,9 @@ int main(void) {
         printf("%"PRIu16"",*arr);
         arr +=1;
         }
-        free(ptr);
     }
 
     else return 1;
-    free(arr);
+    free(inp_ptr);
     return 0;
 }
